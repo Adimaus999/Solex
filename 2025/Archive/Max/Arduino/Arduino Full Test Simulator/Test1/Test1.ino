e228dc9de9a62080aa2e6936ce1b83d147039363
Adimaus999 <maxfitz.zimmer@outlook.com>
2025-03-12 17:32:51 +0000

    changes toarduino

--- 2025/Archive/Max/Arduino/Arduino Full Test Simulator/Test1/Test1.ino
+++ 2025/Archive/Max/Arduino/Arduino Full Test Simulator/Test1/Test1.ino
@@ -19,8 +19,8 @@ struct can_frame frame_velocity;
 struct can_frame frame_acceleration;
 struct can_frame frame_temperature;
 struct can_frame frame_motor_power;
-struct can_frame frame_solar_power;
-struct can_frame frame_aux_power;
+struct can_frame frame_tempvolt;
+struct can_frame frame_solauxmot;
 
 void setup() {
   Serial.begin(115200);
@@ -36,49 +36,33 @@ void setup() {
   
   uint8_t battery_data[8] = {0x20, 0x81, 0x21, 0x01, 0x00, 0x00, 0x00, 0x64};
   uint8_t location_data[8] = {0x40, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD};
-  uint8_t velocity_data[6] = {0x50, 0x00, 0x3C, 0x00, 0x00, 0x00};
+  uint8_t velocity_data[6] = {0x50, 0x00, 0x3C, 0x00, 0x50, 0x3C};
   uint8_t acceleration_data[6] = {0x60, 0xFF, 0x00, 0x01, 0x02, 0x03};
-  uint8_t motor_power_data[8] = {0x80, 0x50, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-  uint8_t solar_power_data[8] = {0x90, 0x10, 0x20, 0x30, 0x00, 0x00, 0x00, 0x00};
-  uint8_t aux_power_data[8] = {0xA0, 0x05, 0x06, 0x07, 0x08, 0x00, 0x00, 0x00};
+  uint8_t tempandvolt[6] = {0x80, 0x50, 0x00, 0xFF, 0x00, 0x00};
+  uint8_t auxsolarmotor[7] = {0xA0, 0x05, 0x06, 0x07, 0x08, 0xFF, 0x00};
   
   initCANFrame(frame_battery, 0x5FF, battery_data, 8);
   initCANFrame(frame_location, 0x071, location_data, 8);
   initCANFrame(frame_velocity, 0x076, velocity_data, 6);
   initCANFrame(frame_acceleration, 0x035, acceleration_data, 6);
-  initCANFrame(frame_motor_power, 0x020, motor_power_data, 8);
-  initCANFrame(frame_solar_power, 0x021, solar_power_data, 8);
-  initCANFrame(frame_aux_power, 0x022, aux_power_data, 8);
+  initCANFrame(frame_tempvolt, 0x010, tempandvolt, 6);
+  initCANFrame(frame_solauxmot, 0x020, auxsolarmotor, 7);
 }
 
 void loop() {
   sendCANMessage(frame_battery);
-  delay(1000);
+  delay(100);
   sendCANMessage(frame_location);
-  delay(1000);
+  delay(100);
   sendCANMessage(frame_velocity);
-  delay(1000);
+  delay(100);
   sendCANMessage(frame_acceleration);
-  delay(1000);
-  
-  sensors.requestTemperatures();
-  float temperature = sensors.getTempCByIndex(0);
-  Serial.println(temperature); // Print the temperature in Celsius
-  uint8_t temperature_data[3] = {0x00, 0x00, 0x00};
-  int temp_milli = static_cast<int>(temperature * 1000);
-  temperature_data[0] = (temp_milli >> 16) & 0xFF;
-  temperature_data[1] = (temp_milli >> 8) & 0xFF;
-  temperature_data[2] = temp_milli & 0xFF;
-  initCANFrame(frame_temperature, 0x010, temperature_data, 3);
-  sendCANMessage(frame_temperature);
-  printCANFrame(frame_temperature); // Print the CAN frame temperature data
-  delay(1000);
-  
+  delay(100); 
   sendCANMessage(frame_motor_power);
-  delay(1000);
-  sendCANMessage(frame_solar_power);
-  delay(1000);
-  sendCANMessage(frame_aux_power);
+  delay(100);
+  sendCANMessage(frame_tempvolt);
+  delay(100);
+  sendCANMessage(frame_solauxmot);
   delay(1000);
 }
 
