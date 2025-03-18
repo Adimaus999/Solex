517d985cd2b43c0bedcb0508b5e8caf2a7edbe1e
henryHcodes <hh573@exeter.ac.uk>
2025-03-18 10:27:15 +0000

    Cap distance remaining to a minimum of 7km
    add in error handling for potential negative distance remaining

--- 2025/Main/Range_Estimation.py
+++ 2025/Main/Range_Estimation.py
@@ -860,7 +860,7 @@ if __name__ == "__main__":
         # Speed strategy optimisation: find the optimal speed to cross the finish line with very low SOC
         # Add a 7km contingency buffer to the calculation to leave approx. 10% charge level at the end of the race
         # Retrieve distance remaining from SQL
-        distanceRemaining = approxRaceLength - (SQLread(30)/1000) + 7
+        distanceRemaining = max(7,(approxRaceLength - (SQLread(30)/1000) + 7))
         #Initialise an array of zeros corresponding to the discrete speeds array used above
         # This will represent the range of the boat at each speed
         rangessArray = np.array([])
